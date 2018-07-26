f36e20f2eacd52fd0c47d542d957871ba8b6f1e1
Anton Melnikov <melnikoffaa@mail.ru>
2018-07-26 23:53:34 +0300

    HW4 Reflection

---
+++ HW4/src/main/java/ru/otus/hw4/Main.java
@@ -0,0 +1,13 @@
+package ru.otus.hw4;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public void test(){
+
+    }
+}
---
+++ HW4/src/test/java/ru/otus/hw4/MainTest.java
@@ -0,0 +1,10 @@
+package ru.otus.hw4;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @org.junit.Test
+    public void test1() {
+    }
+}
