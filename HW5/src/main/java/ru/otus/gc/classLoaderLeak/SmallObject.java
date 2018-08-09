0358f70d216584a30296c640b46d12d4ee279631
Anton Melnikov <melnikoffaa@mail.ru>
2018-08-09 10:52:06 +0300

    GC Listener

--- HW5/src/main/java/ru/otus/gc/classLoaderLeak/SmallObject.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.gc.classLoaderLeak;
-
-public class SmallObject {
-
-    public SmallObject(){
-    }
-}
--- HW5/src/main/java/ru/otus/gc/data/GoodKey.java
+++
@@ -1,8 +0,0 @@
-package ru.otus.gc.data;
-
-public class GoodKey extends Parent {
-
-    public GoodKey(String key){
-        this.key=key;
-    }
-}
