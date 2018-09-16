d7bfcb8f9ced8c0cb2e2a01aec83ccde02c7287b
Anton Melnikov <melnikoffaa@mail.ru>
2018-09-16 19:03:15 +0300

    ATM

---
+++ HW8/src/main/java/ru/otus/atmdepartment/DepartmentAtm.java
@@ -0,0 +1,4 @@
+package ru.otus.atmdepartment;
+
+public interface DepartmentAtm {
+}
---
+++ HW8/src/main/java/ru/otus/atmdepartment/nominals/Bancnote.java
@@ -0,0 +1,27 @@
+package ru.otus.atm.nominals;
+
+public enum Bancnote {
+
+    RUB_10(10), RUB_50(50), RUB_100(100), RUB_200(200),
+    RUB_500(500),RUB_1000(1000), RUB_2000(2000),  RUB_5000(5000);
+
+
+    private int value;
+
+    Bancnote(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    static public Bancnote getNominal(int value) {
+        for (Bancnote nom: Bancnote.values()) {
+            if (nom.getValue() == value) {
+                return nom;
+            }
+        }
+        throw new IllegalArgumentException("unknown value:" + value);
+    }
+}
