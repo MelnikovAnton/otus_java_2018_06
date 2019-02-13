ac2fece10c0a5b489a64f0d6eedde92718a473f8
Anton Melnikov <melnikoffaa@mail.ru>
2019-02-13 23:50:45 +0300

    MessageSystem

--- HW15/src/main/java/ru/otus/hw15/Initializer.java
+++ HW15/src/main/java/ru/otus/hw15/Initializer.java
@@ -1,71 +1,41 @@
 package ru.otus.hw15;
 
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import ru.otus.hw15.frontendService.servlet.*;
+import ru.otus.hw15.frontendService.servlet.filter.EncodingFilter;
 
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.HandlerList;
-import org.eclipse.jetty.server.handler.ResourceHandler;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import ru.otus.hw15.models.AddressDataSet;
-import ru.otus.hw15.models.UserDataSet;
-import ru.otus.hw15.service.DBService;
-import ru.otus.hw15.service.DBServiceHibernateImpl;
-import ru.otus.hw15.servlet.AddUserServlet;
-import ru.otus.hw15.servlet.AllUsersServlet;
-import ru.otus.hw15.servlet.GetByIdServlet;
-import ru.otus.hw15.servlet.TemplateProcessor;
+import javax.servlet.*;
 
-import java.net.URL;
+public class Initializer implements WebApplicationInitializer {
+    @Override
+    public void onStartup(ServletContext servletContext) {
+        // Create the 'root' Spring application context
+        AnnotationConfigWebApplicationContext rootContext =
+                new AnnotationConfigWebApplicationContext();
+        rootContext.register(SpringConfig.class);
 
-public class Initializer {
+        // Manage the lifecycle of the root application context
+        servletContext.addListener(new ContextLoaderListener(rootContext));
 
-    private static Logger logger = LoggerFactory.getLogger(Initializer.class);
-    private final static int PORT = 8090;
-    private final static String PUBLIC_HTML = "tml";
-
-
-    public static void main(String[] args) throws Exception {
-        ResourceHandler resourceHandler = new ResourceHandler();
-        resourceHandler.setResourceBase(Initializer.class.getResource("/").getPath()+PUBLIC_HTML);
-
-        DBService service = new DBServiceHibernateImpl();
-
-        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        TemplateProcessor templateProcessor = new TemplateProcessor();
-
-        UserDataSet user = new UserDataSet("Vasya Pupkin", 21);
-        AddressDataSet address = user.getAddress();
-        address.setStreet("Lenina st");
-        user.addPhone("1234567890");
-        user.addPhone("0987654321");
+        registerServlet(servletContext,"addUser","/addUser",new AddUserServlet());
+        registerServlet(servletContext,"allUsers","/allUsers",new AllUsersServlet());
+        registerServlet(servletContext,"getByID","/getByID",new GetByIdServlet());
+        registerServlet(servletContext,"login","/login",new LoginServlet());
 
 
-        service.save(user);
+
+        FilterRegistration.Dynamic filter = servletContext.addFilter("encodingFilter", EncodingFilter.class);
+        filter.addMappingForUrlPatterns(null,true,"/*");
 
 
-        UserDataSet user1 = service.load(1, UserDataSet.class);
-        logger.warn(user1.toString());
-
-        UserDataSet user2 = new UserDataSet("Ivanov Ivan Ivanovich", 51);
-        AddressDataSet address2 = user2.getAddress();
-        address2.setStreet("Marksa st");
-        user2.addPhone("1111111111");
-        user2.addPhone("2222222222");
-        service.save(user2);
-
-
-        context.addServlet(new ServletHolder(new AllUsersServlet(templateProcessor, service)), "/allUsers");
-        context.addServlet(new ServletHolder(new AddUserServlet(templateProcessor, service)), "/addUser");
-        context.addServlet(new ServletHolder(new GetByIdServlet(templateProcessor, service)), "/getByID");
-
-
-        Server server = new Server(PORT);
-        server.setHandler(new HandlerList(resourceHandler, context));
-
-        server.start();
-        server.join();
     }
 
+    private void registerServlet(ServletContext servletContext, String name, String mapping, Servlet servletInstance){
+        ServletRegistration.Dynamic servlet =
+                servletContext.addServlet(name, servletInstance);
+        servlet.setLoadOnStartup(1);
+        servlet.addMapping(mapping);
+    }
 }
